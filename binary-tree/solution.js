d52fc0eb7963510454040a7ba9c1e5b72ddfa3e1
Punit Shah <punit@punitshah.dev>
2023-07-06 23:12:02 +0100

    Add binary tree test

---
+++ binary-tree/solution.js
@@ -0,0 +1,24 @@
+const getBranchSize = (tree, index) => {
+  // return 0 if node is non-existent
+  if (index > tree.length - 1) return 0; // index is outside array
+  if (tree[index] === -1) return 0; // node value is -1 (representing non-existent node)
+
+  const leftNode = 2 * index + 1;
+  const rightNode = 2 * index + 2;
+
+  const leftBranchSize = getBranchSize(tree, leftNode);
+  const rightBranchSize = getBranchSize(tree, rightNode);
+
+  return tree[index] + leftBranchSize + rightBranchSize;
+};
+
+const solution = (tree) => {
+  const leftBranch = getBranchSize(tree, 1);
+  const rightBranch = getBranchSize(tree, 2);
+
+  if (leftBranch > rightBranch) return 'Left';
+  if (rightBranch > leftBranch) return 'Right';
+  return '';
+};
+
+module.exports = solution;
---
+++ binary-tree/solution.test.js
@@ -0,0 +1,12 @@
+const solution = require('./solution');
+
+test.each`
+  test                                   | input                   | expected
+  ${'Larger left branch (from example)'} | ${[3, 6, 2, 9, -1, 10]} | ${'Left'}
+  ${'Larger right branch'}               | ${[1, 4, 100, 5]}       | ${'Right'}
+  ${'Equal branches'}                    | ${[1, 10, 5, 1, 0, 6]}  | ${''}
+  ${'Empty tree'}                        | ${[]}                   | ${''}
+  ${'Only root node'}                    | ${[1]}                  | ${''}
+`('$test', ({ input, expected }) => {
+  expect(solution(input)).toBe(expected);
+});
